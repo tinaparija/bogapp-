c2c1dd00e641bb8d71eed085790e4bdaf5c35e9b
tinaparija <tinaparija@gmail.com>
2018-01-08 20:07:47 -0800

    updated routes

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -19,4 +19,29 @@ class CreaturesController < ApplicationController
 		creature_id = params[:id]
 		@creature = Creature.find_by_id(creature_id)
 	end 
+
+	def edit 
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+	end 
+
+	def update 
+		creature_id = params[:id]
+		@creature = Creature.find_by_id(creature_id)
+		creature.update_attributes(creature_params)
+		redirect_to creature_path(creature)
+	end 
+
+	def creature_params 
+		params.require(:creature).permit(:name, :description)
+	end 
+
+	def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+ 	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 	get "/creatures/new", to: "creatures#new", as: "new_creature"
 	post "/creatures" => "creatures#create"
 	get "/creatures/:id", to: "creatures#show", as: "creature"
+	get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+	patch "/creatures/:id", to: "creatures#update"
+	delete "/creatures/:id", to: "creatures#destroy"
 end
